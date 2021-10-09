603564ed63771fd4051db75316f7aa05729670ac
dalton santistevan <daltonskaterboard@outlook.com>
2021-10-09 15:11:10 -0500

    conf base ip

--- views/dist/js/scripts/base.js
+++ views/dist/js/scripts/base.js
@@ -1,2 +1,2 @@
-const urlServidor = "http://localhost/hr_orrala/backend/";
-const urlCliente = "http://localhost/hr_orrala/frontend/";
+const urlServidor = "http://192.168.100.213/hr_orrala/backend/";
+const urlCliente = "http://192.168.100.213/hr_orrala/co-c1-front/";
